b04b12878593ebea05726583d148ad0520d6a8ae
myk_eshun <dev.mike.eshun@gmail.com>
2018-09-21 20:49:01 +0000

    components, methods, custom event and relevant props

--- app/main.js
+++ app/main.js
@@ -1,10 +1,52 @@
 const inputComponent = {
-  template: `<input class="input is-small" type="text" />`
-}
+  template: `
+    <input 
+      class="input is-small" 
+      type="text" 
+      :placeholder="placeholder"
+      v-model="input"
+      @keyup.enter="monitorEnterKey"/>
+    `,
+    props: ['placeholder'],
+    data: () => ({
+      input: ''
+    }),
+    methods: {
+      monitorEnterKey() {
+        this.$emit('add-note', {
+          note: this.input,
+          timestamp: new Date().toLocaleString(),
+        });
+        this.input = '';
+      }
+    },
+};
+
+const noteCountComponent = {
+  template: `
+    <div class="column has-text-centered">
+      Note count: {{ notes.length }}
+    </div>
+    `,
+    props: ['notes'],
+};
 
 new Vue({
   el: '#app',
+  data: {
+    count: 0,
+    notes: [],
+    timestamps: [],
+    placeholder: 'Enter a note'
+  },
   components: {
-    'input-component': inputComponent
-  }
-})
+    'input-component': inputComponent,
+    'note-count-component': noteCountComponent,
+  },
+  methods: {
+    addNote(event) {
+      this.notes.push(event.note);
+      this.timestamps.push(event.timestamp);
+    },
+  },
+});
